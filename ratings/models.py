10bf846d6ceccd1e51e02737ba76492a48c95fda
Tim Claes <tim.claes@me.com>
2015-12-28 19:58:20 +0100

    Added Vote and Score model classes

--- ratings/models.py
+++ ratings/models.py
@@ -6,6 +6,52 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
+class Score(models.model):
+
+	# Rating system makes use of contenttypes framework!
+	content_type = models.ForeignKey(ContentType)
+	object_id = models.PositiveIntegerField()
+	content_object = GenericForeignKey('content_type', 'object_id')
+
+	total_score = models.IntegerField(default=0)
+	votes_count = models.PositiveIntegerField(default=0)
+	average = models.FloatField(default=0)
+
+	class Meta:
+
+		unique_together = (
+			('content_type', 'object_id')
+		)
+
+	def __unicode__(self):
+
+		return 'Score voor %s' % (self.content_object, )
+
+	def get_votes(self):
+		'''
+		Returns all related votes 
+		'''
+
+		return Vote.objects.filter(content_type=self.content_type, object_id=self.object_id)
+
+	def recalculate(self, commit=True):
+
+		# Score is updated by aggregating Vote data
+		# total_score is sum of all Vote scores, and count is number of Vote objects (id is pk, so unique)
+		current_score = self.get_votes().aggregate(total_score=models.Sum('score'), votes_count=models.Count('id'))
+
+		self.total_score = current_score['total_score'] or 0
+		self.votes_count = current_score['votes_count'] 
+
+		if self.votes_count:
+			self.average = self.total_score / self.votes_count
+
+		else:
+			self.average = 0
+
+		if commit:
+			self.save()
+
 
 class Vote(models.Model):
 
