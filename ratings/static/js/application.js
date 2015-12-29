e93c585c2c141b10f315691b3724e01861ef4646
Tim Claes <tim.claes@me.com>
2015-12-29 21:00:00 +0100

    Starting to shape functionality

--- ratings/static/js/application.js
+++ ratings/static/js/application.js
@@ -1,5 +1,36 @@
 /* Product page: Quotation */
 $.fn.rate_me = function() {
 
-	
+	// 1. User presses submit button
+	$('#submit-rating').click(function(e){
+
+		e.preventDefault();
+
+		var dict = {};
+
+		$('#rating-form :input[type=custom]').each(function(){
+
+			console.log('jeej');
+
+			dict[$(this).attr('name')] = $(this).val();
+
+			/*dict.push({
+				key: $(this).attr('name'),
+				value: $(this).val()
+			});*/
+
+		});	
+
+		console.log(dict);
+
+		
+		console.log('submit button pressed!')
+
+		// 2. Check values
+		eerste = $('#input-1').val();
+		tweede = $('#input-2').val();
+
+		console.log(eerste + '--' + tweede)
+
+	});
 };
